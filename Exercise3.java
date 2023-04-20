4b0e86c9b2cc1346e9d996d1a2f4095c24c51677
Georgi Iliev <57075844+gopeto22@users.noreply.github.com>
2023-04-20 10:49:51 +0100

    Update Exercise3.java

--- Exercise3.java
+++ Exercise3.java
@@ -3,8 +3,12 @@ import java.util.*;
 public class CarrotRabbit {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //reads the size of the garden and the number of carrots in each cell of the garden from the input
         int n = scanner.nextInt();
         int[][] garden = new int[n][n];
+        //depth-first search (DFS) algorithm to count the number of groups of carrots
+        //starts at the top left corner of the garden and visits all the cells in each 
+        //group of carrots, marking them as visited by setting their value to 0
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 garden[i][j] = scanner.nextInt();
@@ -13,8 +17,11 @@ public class CarrotRabbit {
         int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
+                //visited all the cells in a group
                 if (garden[i][j] > 0) {
+                    //increments the count of groups
                     count++;
+                    //continues searching for the next group until it has visited all the cells in the garden
                     dfs(garden, i, j);
                 }
             }
